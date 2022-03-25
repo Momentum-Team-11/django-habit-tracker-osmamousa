42646be7207fe691eb3549ced9af6143f1ed42c3
Osama_mousa <osamamousa048@gmail.com>
2022-03-25 11:01:20 -0400

    first steps

--- habit/models.py
+++ habit/models.py
@@ -13,13 +13,13 @@ class User(AbstractBaseUser):
 
 
 class Habit(models.Model):
-    habit_name = models.CharField(max_length=50)
-    habit_goal = models.CharField(max_length=200)
+    name = models.CharField(max_length=50)
+    goal = models.CharField(max_length=200)
     user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="habit_user"
     )
 
 
-class record(models.Model):
+class Record(models.Model):
     date = models.DateField(auto_now_add=datetime.now)
     habit = models.ForeignKey(
         Habit, on_delete=models.CASCADE, null=True, blank=True, related_name="habit"
--- habit/views.py
+++ habit/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+import imp
+from django.shortcuts import render, redirect, get_list_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Habit, User, Record
+# from .forms import HabitForm, RecordFrom
 
 # Create your views here.
+
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect ("home.html")
+    else:
+        return render(request, "login.html")
+
+def home(request):
+    habits = Habit.objects.all()
+    return render (request, "home.html",)
